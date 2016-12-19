9cdf41e46006a11a677d54da107934b4c2b8777a
demkinev <evgen1000end@gmail.com>
2016-12-19 19:52:03 +0300

    refactoring

--- src/main/java/ru/demkin/Main.java
+++ src/main/java/ru/demkin/Main.java
@@ -4,7 +4,14 @@ package ru.demkin;
  * @author <a href="mailto:evgen1000end@gmail.com">demkinev</a>
  */
 public class Main {
-  public static void main(String[] args) {
+
+  private static void print(){
     System.out.println("Some changes in feature branch");
   }
+
+  public static void main(String[] args) {
+
+  }
+
+
 }
