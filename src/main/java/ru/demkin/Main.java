e39b3afa9696616621b2f92bc027f688ce75e3c0
demkinev <evgen1000end@gmail.com>
2016-12-19 19:20:40 +0300

    Initial commit

---
+++ src/main/java/ru/demkin/Main.java
@@ -0,0 +1,10 @@
+package ru.demkin;
+
+/**
+ * @author <a href="mailto:evgen1000end@gmail.com">demkinev</a>
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Test");
+  }
+}
