6c8d42e080c83d4643ce7536019685ad4bad8190
demkinev <evgen1000end@gmail.com>
2016-12-19 19:32:37 +0300

    important feature

--- src/main/java/ru/demkin/Main.java
+++ src/main/java/ru/demkin/Main.java
@@ -5,6 +5,6 @@ package ru.demkin;
  */
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Test");
+    System.out.println("Some changes in feature branch");
   }
 }
