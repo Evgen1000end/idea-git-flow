aeb5e2e66999239b8d8a69df74e29feb3e5fcfe5
demkinev <evgen1000end@gmail.com>
2016-12-19 19:50:51 +0300

    some changes in master

--- src/main/java/ru/demkin/Main.java
+++ src/main/java/ru/demkin/Main.java
@@ -4,7 +4,14 @@ package ru.demkin;
  * @author <a href="mailto:evgen1000end@gmail.com">demkinev</a>
  */
 public class Main {
+
+
   public static void main(String[] args) {
     System.out.println("Some changes in feature branch");
   }
+
+  public static void methodInMaster(){
+    System.out.println("into master");
+  }
+
 }
